70abb5f59c74b79804ca86b0cb510e2934643941
Eliav Shames <101091197+eliavs-snyk@users.noreply.github.com>
2022-07-24 11:51:04 +0300

    Update index.js

--- workspace-b/index.js
+++ workspace-b/index.js
@@ -4,6 +4,6 @@ const serve = require('workspace-a/lib').serve;
 serve(cowsay.say({
     text: "Hellooo froom workspace-b!",
     e: "oO",
-    T: "U "
+    T: "U "//bvkeiorgiorjt
 }))
 //another one
